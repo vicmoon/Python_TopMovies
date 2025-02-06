37427df0316cebeed3261b61344b46c593b10100
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2025-02-06 18:54:19 +0100

    update

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, Column
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my-movies-collection.db"
+
+# init SQLAlchemy with the app 
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 
-# CREATE TABLE
+#Define a module 
+
+class Movie(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    year = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    review = Column(String(250), nullable=False)
+    img_url = Column(String(250), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('title', name='uq_movie_title'),
+    )
+
+#create tables 
+
+with app.app_context():
+    db.create_all()
+
+#add new entry 
+
+with app.app_context():
+    new_movie = Movie(title="New Movie", year="2025", description="The Description", review="The Review", img_url="https://image.tmdb.org0.jpg")
+    db.session.add(new_movie)
+    db.session.commit()
+
 
 
 @app.route("/")
